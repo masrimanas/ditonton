632f9e446a83d01224640f3ba78da24306d96535
masrimanas <saya@masrimanas.com>
2022-05-01 16:06:27 +0700

    add a test for CodeMagic

---
+++ test/main_test.dart
@@ -0,0 +1,45 @@
+import 'package:core/data/models/movie_model.dart';
+import 'package:core/domain/entities/movie.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+// This lines of code are used to avoid CodeMagic Test failure
+void main() {
+  final tMovieModel = MovieModel(
+    adult: false,
+    backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+    genreIds: [14, 28],
+    id: 557,
+    originalTitle: 'Spider-Man',
+    overview:
+        'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+    popularity: 60.441,
+    posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+    releaseDate: '2002-05-01',
+    title: 'Spider-Man',
+    video: false,
+    voteAverage: 7.2,
+    voteCount: 13507,
+  );
+
+  final tMovie = Movie(
+    adult: false,
+    backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+    genreIds: [14, 28],
+    id: 557,
+    originalTitle: 'Spider-Man',
+    overview:
+        'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+    popularity: 60.441,
+    posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+    releaseDate: '2002-05-01',
+    title: 'Spider-Man',
+    video: false,
+    voteAverage: 7.2,
+    voteCount: 13507,
+  );
+
+  test('should be subclass of Movie entity', () async {
+    final result = tMovieModel.toEntity();
+    expect(result, tMovie);
+  });
+}
